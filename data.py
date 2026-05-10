6a80131bf1d0c72f8b7f2b325d71c86e4c45c1aa
PIYUSH SONI 🕸 <piyushsoni7777777@gmail.com>
2026-05-10 15:04:35 +0530

    Add developer comment in data.py
    Added a comment indicating the developer's name.

--- data.py
+++ data.py
@@ -10,3 +10,4 @@ response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
+# develope by piyushsoni
